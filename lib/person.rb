721899d69a1145bb4a5867540f27c529d12ab7f1
aidyn1984 <csa.aiden@gmail.com>
2021-02-06 22:14:27 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+#require 'pry'
+
+class Person
+    def initialize(name)
+        @name = name
+    end
+    def name= (name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+end
+
+#binding.pry
+#0
